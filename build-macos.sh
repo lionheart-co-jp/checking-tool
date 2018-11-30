28608face73daf5de2795f98f49c50be7cba6f57
SUSH <sush@sus-happy.net>
2018-11-30 23:24:31 +0800

    Update gitlab ci

--- build-macos.sh
+++ build-macos.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 APP="CheckTool.app"
-mkdir -p $APP/Contents/{MacOS,Resources}
+mkdir -p $APP/Contents/MacOS
+mkdir -p $APP/Contents/Resources
 npx pkg package.json --targets node8-macos-x64 -o $APP/Contents/MacOS/CheckTool
 cat > $APP/Contents/Info.plist << EOF
 <?xml version="1.0" encoding="UTF-8"?>
